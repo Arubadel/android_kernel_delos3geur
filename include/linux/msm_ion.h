a769a9c86b2ebfd4cce3fc0c7e5375833033059a
grace5921 <androidlover5842@gmail.com>
2017-05-18 09:48:51 +0530

    msm7x27a: hack caf display build
    tmp patch to work around caf display build errors

---
+++ include/linux/msm_ion.h
@@ -0,0 +1,24 @@
+/*
+ * include/linux/ion.h
+ *
+ * Copyright (C) 2011 Google, Inc.
+ * Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _LINUX_ION_H
+#define _LINUX_ION_H
+
+#include <linux/ion.h>
+
+
+#endif /* _LINUX_ION_H */
